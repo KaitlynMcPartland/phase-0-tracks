cfc196711cbce93bc20f0229c10d448b5c6ce097
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-04 19:11:11 -0400

    Add prompts to get name, age, birth year, garlic preference, and insurance preference from user in vampires.rb for my solo challenge 4.4.

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+def vampire_detector
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+wants_insurance = gets.chomp
+
+
+
+end
