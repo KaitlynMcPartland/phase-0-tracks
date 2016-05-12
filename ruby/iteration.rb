434f9ed051f90ede2b0b47b57511e0c219ab06a9
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-12 14:33:59 -0400

    Add practice array and hash to iterate through using each and map for Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,65 @@
-def morning_bev 
-  puts "Here we are at the beginning of the method."
-  yield("coffee")
-  puts "Here we are at the end of the method."
+# def morning_bev 
+#   puts "Here we are at the beginning of the method."
+#   yield("coffee")
+#   puts "Here we are at the end of the method."
+# end
+# morning_bev { |drink| puts "I like #{drink}"}
+
+
+symptoms = ["sneezing", "fever", "sore knee", "sore throat"]
+
+instruments = {
+	cutting_device: "scalpel",
+	closure_device: "needle",
+	suction_device: "vacuum",
+	anesthesia_delivery: "face mask"
+}
+
+#each array iteration practice
+puts symptoms
+symptoms.each do |sympt|
+	puts "My symptom is #{sympt}"
 end
-morning_bev { |drink| puts "I like #{drink}"}
+puts symptoms
+
+
+#each hash iteration practice
+puts instruments
+instruments.each do |device, equip|
+	puts "The #{device} is a #{equip}"
+end
+puts instruments
+
+
+#map array iteration practice
+
+puts symptoms
+symptoms.map! do |sympt|
+	"intense " + sympt
+end
+
+puts symptoms
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
