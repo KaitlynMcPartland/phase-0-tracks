3fd192a25f8ba24225662567589997bf8f2cfaeb
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-16 09:06:30 -0400

    Add car hash full of 3 cars each with a hash of passengers and an array of their clothing for exercise 5.5

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,22 @@
+###Make a hash of cars
+##Each car value is a hash of passengers
+#Each passenger value is an array or clothing
+
+car_hash = {
+	car_1: {
+		kara: ['hat', 'shirt', 'skirt'],
+		john: ['sunglasses', 'sweater', 'jeans']
+	},
+	car_2: {
+		kaitlyn: ['sunglasses', 'snapback', 'shorts', 'tank top'],
+		connor: ['snapback', 'tank', 'shorts']
+	},
+	car_3: {
+		heidi: ['glasses', 'blouse', 'pants'],
+		kevin: ['glasses', 'polo', 'jeans'],
+		sara: ['bonnet', 'diaper', 'dress']
+	}
+	
+}
+
+p car_hash
