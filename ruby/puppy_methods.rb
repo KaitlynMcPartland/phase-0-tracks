d2fb87c477419e5a3e2986ebe4020e9d77f68937
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-17 20:24:49 -0400

    Initial commit - copied Puppy and Cat class from 6.2 pairing session

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,68 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    i = 0
+    while i < num.to_i
+      puts "woof!"
+      i += 1
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    human_years * 4
+  end
+
+  def chews(item)
+    puts "#{item} is chewed!"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..." 
+  end
 end
 
+class Cat
+
+  def initialize
+    puts "Here's a new cat!"
+  end
+
+  def meow
+    puts "Meow!"
+  end
+
+  def scratches(item)
+    puts "#{item} is scratched!"
+  end
+
+end
+
+finn = Cat.new
+finn.meow
+finn.scratches("Couch")
+
+cat_array = []
+50.times do |x|
+  cat_array.push(x = Cat.new)
+end
+
+cat_array.each do |cat|
+  cat.meow
+  cat.scratches("Shin")
+end
+
+
+
+=begin
+spot = Puppy.new
+spot.fetch("ball") 
+spot.speak(3)
+spot.roll_over
+puts spot.dog_years(8)
+spot.chews("Ethernet cord")
+=end
