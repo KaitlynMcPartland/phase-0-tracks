8f2e8dbd69072042df63f2d0238477d72e3aa47f
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-17 21:37:54 -0400

    Add attribute changing methods and getter methods to Santa class
    Add getter methods for all attributes and setter methods for birthday, reiandeer_ranking and gender

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+#initialize method
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
@@ -7,6 +8,34 @@ class Santa
 		@age = 0
 	end
 
+#getter methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def gender
+		@gender
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+#setter methods
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking << reindeer
+	end
+
+#other methods
 	def speak
 		puts "Ho, ho, ho! Haaaaappy holidays!"
 	end
@@ -15,25 +44,52 @@ class Santa
 		puts "That was a good #{cookie_type}"
 	end
 
+	def celebrate_birthday
+		@age = @age + 1
+	end
+
+
+
 end
 
+
+#Driver Code
+
+##Release 0
 # test_santa = Santa.new
 # test_santa.speak
 # test_santa.eat_milk_and_cookies("chocolate chip")
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
 
-cookies = ["chocolate chip", "sugar", "oatmeal", "raisin", "peanut butter", "snickerdoodle", "walnut"]
+##Release 1
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
-santas.each_with_index do |cur_santa, i|
-	cur_santa.speak
-	cur_santa.eat_milk_and_cookies(cookies[i])
-end
+# cookies = ["chocolate chip", "sugar", "oatmeal", "raisin", "peanut butter", "snickerdoodle", "walnut"]
+
+# santas.each_with_index do |cur_santa, i|
+# 	cur_santa.speak
+# 	cur_santa.eat_milk_and_cookies(cookies[i])
+# end
+
+
+##Release 2
+# test_santa = Santa.new("female", "white")
+# p test_santa.age
+# test_santa.celebrate_birthday
+# test_santa.celebrate_birthday
+# p test_santa.age
+# p test_santa.ethnicity
+# p test_santa.gender
+# test_santa.gender = "male"
+# p test_santa.gender
+# p test_santa.reindeer_ranking
+# test_santa.get_mad_at("Dancer")
+# p test_santa.reindeer_ranking
 
