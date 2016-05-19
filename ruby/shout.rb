1bd905774fc062f608a41983f32b3647e328b15a
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-19 16:07:19 -0400

    Add mixin Shout module and classes for Man and Child

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,48 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yelling_angrily(words)
+		puts words.upcase + "!" + " :("
 	end
 
-	def self.yelling_happily(words)
-		words + "!" + " :)"
+	def yelling_happily(words)
+		puts words.upcase + "!" + " :)"
 	end
 end
 
-##Driver Code
-p Shout.yell_angrily("Boo, I'm so angry")
+class Man
+	include Shout
+end
 
-p Shout.yelling_happily("Yay, I'm so happy")
+class Child
+	include Shout
+end
+
+
+##Driver Code
+man = Man.new
+man.yelling_angrily("I dont like you")
+man.yelling_happily("I really like you")
+
+child = Child.new
+child.yelling_angrily("I want candy")
+child.yelling_happily("I love candy")
+
+
+
+
+
+
+#Old Code
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!" + " :)"
+# 	end
+# end
+
+# ##Driver Code
+# p Shout.yell_angrily("Boo, I'm so angry")
+
+# p Shout.yelling_happily("Yay, I'm so happy")
