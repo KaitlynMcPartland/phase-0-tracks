ec74f225836e49b9b4dd596be2883c428ca65de0
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-19 21:07:25 -0400

    Add methods create, add, delete and print items from a grocery list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,64 @@
+# GPS 2.2 - Grocery List
+
+# Create a new list:
+
+# Input: list of items (example: "carrots apples cereal pizza")
+# items=("carrots apples cereal pizza")
+# Iterate .each Key-Value pair
+# groceries{item => quantity}
+
+def create_new_list(list_items)
+	grocery_list = Hash.new
+	list_items.split.each do |item|
+		grocery_list[item] = 1
+	end
+	grocery_list
+end
+
+
+# Hash: Key-Value: Carrots => 12
+# Default quanity: 1
+
+# Output: key-value ... ie .. print out Carrots: 12, etc .. item:quantity
+
+# Add an item with a quantity to the list
+# def add_or_update_item(item,quantity)
+def add_or_update_item(grocery_list,item,quantity=1)
+# default quanity=1
+# groceries[item]=quanity
+	grocery_list[item] = quantity
+	grocery_list
+end
+
+# Remove an item from the list
+# def remove_item(item)
+def remove_item(grocery_list,item)
+# groceries.delete(item)
+	grocery_list.delete(item)
+	grocery_list
+end
+
+
+# Print the list (Consider how to make it look nice!)
+def print_list(grocery_list)
+# groceries.each {|item,quantity| puts #{item}: #{quantity}}
+	grocery_list.each {|item,quantity| puts "#{item}: #{quantity}"}
+
+end
+
+##Driver code
+grocery_list = create_new_list("carrots apples cereal pizza")
+p grocery_list
+add_or_update_item(grocery_list, "carrots", 12)
+p grocery_list
+add_or_update_item(grocery_list, "blueberries")
+p grocery_list
+remove_item(grocery_list, "carrots")
+print_list(grocery_list)
+
+
+
+
+
+
+
