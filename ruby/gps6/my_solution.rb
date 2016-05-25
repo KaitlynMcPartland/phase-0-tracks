e9d4c031d361427b80d8eca12e1fa1428e3b585a
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-24 21:21:50 -0400

    Refactor speed_of_spread and predicted_deaths methods in VirusPredictor class

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -31,17 +31,33 @@ class VirusPredictor
   #instance variable @population_density for the instance
   def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
+    # if @population_density >= 200
+    #   number_of_deaths = (@population * 0.4).floor
+    # elsif @population_density >= 150
+    #   number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+    #   number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+    #   number_of_deaths = (@population * 0.1).floor
+    # else
+    #   number_of_deaths = (@population * 0.05).floor
+    # end
+
+    #defined number in original if else method as death ratio (aka .4 or .3 or .2)
+    #changed the death ratio from a discrete range od numbers to a continuous funtion
+    #using the equation for a line to get more accurate number of deaths
+    #b = .05
+    #y = @population_density
+    #x = death_ration
+    # m = 571
+    # x = y/m - b
+
+    death_ratio = @population_density / 571
+    if death_ratio > 1
+      death_ratio = 0.99
     end
+
+    number_of_deaths = (@population * death_ratio).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
@@ -52,21 +68,35 @@ class VirusPredictor
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+    # speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    # if @population_density >= 200
+    #   speed += 0.5
+    # elsif @population_density >= 150
+    #   speed += 1
+    # elsif @population_density >= 100
+    #   speed += 1.5
+    # elsif @population_density >= 50
+    #   speed += 2
+    # else
+    #   speed += 2.5
+    # end
+
+    #changed speed var name to months
+    #changed the months from a discrete range of numbers to a continuous funtion
+    #using the equation for a line to get more accurate timeline
+
+    # y = speed
+    # x = @population_density
+    # slope = -1/75
+    # b = 2.5
+
+    months = @population_density * (-1.0/75) + 2.5
+    if months < 0
+      months = 0
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    puts " and will spread across the state in #{months} months.\n\n"
 
   end
 
@@ -84,6 +114,8 @@ end
 
 
 
+# wash = VirusPredictor.new("Washington,D.C.", STATE_DATA["Washington,D.C."][:population_density], STATE_DATA["Washington,D.C."][:population])
+# wash.virus_effects
 
 # alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 # alabama.virus_effects
