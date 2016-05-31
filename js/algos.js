b50d82d9f596ac2702c4a197e1733ec569e1a1b4
KaitlynMcPartland <kmcp921@gmail.com>
2016-05-31 11:39:03 -0400

    Add keyValueMatch function
    Method iterates through two objects and returns true if any of the key-value pairs in the two objects match, otherwise function returns false

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,9 @@
 //longest phrase in array method
-//method will return longest phrase - if two are same length will return both
+//method will return longest phrase
 //create a an empty variable for longest current phrase
 //loop through the array
 	// if current phrase.length is > longest current phrase
 	// set longest current phrase = current phrase
-
-	// shovel current word into array
-	// else continue to next phrase
 //return longest current phrase array
 
 function longestPhrase (arrayOfPhrases) {
@@ -22,5 +19,51 @@ function longestPhrase (arrayOfPhrases) {
 	return curLongestPhrase;
 }
 
-console.log(longestPhrase(["firstest phrase", "second phrase", "longest phrase for sure", "fourth"]));
-console.log(longestPhrase(["short", "medium", "other", "longest"]))
+//compare two objects method
+//iterate through first object using a for loop
+//within the for loop code block iterate through the second object (nestediteration)
+//if obj1 key === obj2 key AND obj1  value === obj2 value return true
+//otherwise continue iteration
+
+
+function keyValueMatch (firstObject, secondObject) {
+	for (var firstObjKey in firstObject) {
+		for (var secondObjKey in secondObject){
+			if ((firstObjKey === secondObjKey) && (firstObject[firstObjKey] === secondObject[secondObjKey])) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+//driver code/ tests
+//longestPhrase tests
+
+//console.log(longestPhrase(["firstest phrase", "second phrase", "longest phrase for sure", "fourth"]));
+//console.log(longestPhrase(["short", "medium", "other", "longest"]))
+
+
+
+//keyValueMatch tests
+
+// console.log(keyValueMatch ({name: "Steven", age: 54}, {name: "Tammy", age: 54}));
+// console.log(keyValueMatch ({name: "Steven", age: 20}, {name: "Tammy", age: 54}));
+// console.log(keyValueMatch ({job: "lawyer", school: "Duke", position: "associate"}, {name: "Tammy", age: 54}));
+// console.log(keyValueMatch ({job: "lawyer", school: "Duke", position: "associate", age: 54}, {name: "Tammy", age: 54}));
+// console.log(keyValueMatch ({job: "lawyer", school: "Duke", position: "associate", 'age': 54}, {name: "Tammy", age: 54}));
+
+
+
+
+
